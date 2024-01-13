40f8b10e56b9a47d5b560d1df87a05654038e748
Timothy Guo <timothyguo86@gmail.com>
2024-01-12 22:43:59 -0500

    (feat)Added webpack dev server and hot module reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devServer: {
+    static: './dist'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development'
+    })
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
