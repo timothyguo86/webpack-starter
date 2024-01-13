9223a1ee68c29ffe7a8b82952817b934d3edae1e
Timothy Guo <timothyguo86@gmail.com>
2024-01-12 23:25:44 -0500

    (feat) Set up Babel

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,20 @@ module.exports = {
   devServer: {
     static: './dist'
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Development'
