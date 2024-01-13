ae4dcb3d4cde178832adaa535c2c523c664a3456
Timothy Guo <timothyguo86@gmail.com>
2024-01-13 18:11:06 -0500

    (refactor) Cleanup

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,4 @@
 import _ from 'lodash'
 import '../styles/main.scss'
 
-console.log(_.join(['Index', 'module', 'loaded!'], ' '))
-let myName = 'Tim Guo'
-
-const hello = name => {
-  return `Hello ${name}`
-}
-
-const q1 = ['Jan', 'Feb', 'Mar']
-const q2 = ['Apr', 'May', 'Jun']
-const q3 = ['Jul', 'Aug', 'Sep']
-const q4 = ['Oct', 'Nov', 'Dec']
-
-const year = [...q1, ...q2, ...q3, ...q4]
-
-console.log(year)
-
-const h1 = document.createElement('h1')
-const p = document.createElement('p')
-
-h1.innerText = `${hello(myName)}`
-p.innerText = `${year}`
-p.classList.add('example')
-
-document.querySelector('body').appendChild(h1)
-document.querySelector('body').appendChild(p)
+console.log(_.join(['Hello', 'World!'], ' '))
