56c1d4e9e6ba919aec0b8c2107975debf6afcfa8
Timothy Guo <timothyguo86@gmail.com>
2024-01-28 14:32:11 -0500

    (feat) Turn off eslint error message for usage of unary operator '++'

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,25 +27,26 @@ module.exports = {
      * "error" or 2 - Enable the rule as an error (program exits when triggered)
      */
 
-    'no-undef': process.env.NODE_ENV === 'production' ? 'warn' : 'off', // Turn off warnings for all undefined variables
-    'no-console': 1, // Display warnings for console.log() syntax
-    indent: 'off',
-    semi: [2, 'never'], // Require or disallow semicolons instead of ASI; 'never' disallows them
-    quotes: [2, 'single'], // Use single quotes
-    'no-mixed-spaces-and-tabs': [2], // Disallow mixed spaces and tabs for indentation
-    'no-extra-semi': [2], // Disallow unnecessary semicolons
     'comma-dangle': [2, 'never'], // Disallow trailing commas
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }], // Add exceptions for [no-extraneous-dependencies]
+    indent: 0,
+    'no-console': 1, // Display warnings for console.log() syntax
     'no-dupe-args': 2, // Disallow duplicate arguments in function definitions
     'no-dupe-keys': 2, // Disallow duplicate keys in object literals
     'no-duplicate-case': 2, // Disallow a duplicate case label within a switch statement
     'no-empty': 2, // Disallow empty block statements
+    'no-extra-semi': 2, // Disallow unnecessary semicolons
     'no-func-assign': 2, // Disallow overwriting functions declared as variables
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true }], // Add exceptions for [no-extraneous-dependencies]
+    'no-mixed-spaces-and-tabs': 2, // Disallow mixed spaces and tabs for indentation
+    'no-plusplus': 0, // Allow usage of unary operator '++'
+    'no-undef': process.env.NODE_ENV === 'production' ? 'warn' : 'off', // Turn off warnings for all undefined variables
     'prettier/prettier': [
       'error',
       {
         endOfLine: 'auto' // Disable prettier from enforcing a specific line ending format
       }
-    ]
+    ],
+    quotes: [2, 'single'], // Use single quotes
+    semi: [2, 'never'] // Require or disallow semicolons instead of ASI; 'never' disallows them
   }
 }
