8ec13492284f8ba6b9d9f777345b3c9ee75c17dc
Timothy Guo <timothyguo86@gmail.com>
2024-02-17 23:32:21 -0500

    [docs] Updated demo page

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,9 @@
 import '../styles/main.scss'
 
+const helloWorld = document.createElement('h2')
+helloWorld.textContent = 'Hello World'
+document.body.appendChild(helloWorld)
+
 const sum = (a, b) => a + b
 
 console.log('sum::: ', sum(1, 2)) // eslint-disable-line no-console
